04b4f743a9c8d0de9b92edc27f90806cb1cbcf0b
sambare <boubasambare@yahoo.it>
2020-03-17 23:04:59 +0100

    authentification middleware

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const server = express();
 const cors = require("cors");
+const passport = require('passport');
 const listEndPoints = require("express-list-endpoints");
 const connectMongoose = require("./src/db/mongodb")
+const authRouter = require('./src/route/authRouter')
 const User = require("./src/model/userSchema")
 const dotenv = require("dotenv");
 dotenv.config();
 
 server.use(express.json()); // without this, you cannot post
+server.use(passport.authenticate())
 server.use(cors());
+
+server.use("/api/auth", authRouter);
 
 server.get("/", passport.authenticate("jwt"), async(req, res) => {
   res.send(await User.find())
---
+++ src/middlewares/authentificationMiddleware.js
@@ -0,0 +1,34 @@
+const LocalStrategy = require('passport-local').Strategy
+const JwtStrategy = require('passport-jwt').Strategy
+const ExtractJwt = require('passport-jwt').ExtractJwt
+const passport = require('passport')
+const UserModel = require('../model/userSchema')
+const jwt = require('jsonwebtoken')
+const dotenv = require('dotenv')
+dotenv.config()
+
+passport.serializeUser(UserModel.serializeUser())
+passport.deserializeUser(UserModel.deserializeUser())
+
+passport.use(new LocalStrategy(UserModel.authenticate()))
+
+const password = process.env.JWT_SECRET
+const jwtConfig = {jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), secretOrKey: password}
+
+passport.use(new JwtStrategy(jwtConfig,(jwtPayload,next) => {
+    UserModel.findById(jwtPayload._id, (err, user) => {
+        if (err) return next(err, null)
+        else if (user) return next(null, user)
+        else return next(null, false)
+    })
+}))
+
+
+
+
+
+
+
+module.exports = {
+    generateToken: user => jwt.sign(user,password,{expiresIn:4000})
+}
