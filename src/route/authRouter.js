09a7c2510281cce4476337432c191c825fcbfb3e
sambare <boubasambare@yahoo.it>
2020-03-23 01:50:59 +0100

    add cryptojs

--- src/route/authRouter.js
+++ src/route/authRouter.js
@@ -1,12 +1,13 @@
 const { Router } = require('express')
 const passport = require('passport')
+const CryptoJS = require("crypto-js");
 const { generateToken } = require('../middlewares/authentificationMiddleware')
 const { verifyEmail } = require('../middlewares/verifyEmail')
 const { sendEmail } = require('../middlewares/sendEmail')
 const UserModel = require('../model/userSchema')
 const router = Router()
 
-router.get("/",  async (req, res) => {
+router.get("/", async (req, res) => {
     try {
         res.send(await UserModel.find({}))
     } catch (error) {
@@ -17,22 +18,24 @@ router.get("/",  async (req, res) => {
 router.post("/register", async (req, res) => {
     try {
         const user = await UserModel.register(req.body, req.body.password)
-        let subject = "Account Verification Token"
+        const username = await CryptoJS.AES.encrypt(user.username, process.env.CRYPTO_SECRET).toString();
+        let subject = "APPCADEMIX Account Verification Token"
         let to = user.email
         let from = process.env.FROM_EMAIL
-        let link=`${req.protocol}://${req.get('host')}/api/auth/verify/${user.username}`
+        let link = `${req.protocol}://${req.get('host')}/api/auth/verify/${username}`
+        console.log(link)
         let html = `<p>Hi ${user.username}<p><br><p>Please click on the following <a href="${link}">link</a> to verify your account.</p> 
                   <br><p>If you did not request this, please ignore this email.</p>`
 
-        await sendEmail({to, from, subject, html});
-        res.status(200).json({message:'You have been successfully registered check your email activate your account'})
-        
+        await sendEmail({ to, from, subject, html });
+        res.status(200).json({ message: 'You have been successfully registered check your email activate your account' })
+
     } catch (error) {
         res.status(500).send(error.message)
     }
 })
 
-router.post("/login",verifyEmail(), passport.authenticate('local'), async (req, res) => {
+router.post("/login", verifyEmail(), passport.authenticate('local'), async (req, res) => {
     try {
         const token = generateToken({ _id: req.user._id, username: req.user.username })
         res.send({ access_token: token, username: req.user.username })
@@ -52,12 +55,14 @@ router.post("/refreshtoken", passport.authenticate('jwt'), async (req, res) => {
 
 router.get('/verify/:username', async (req, res) => {
     try {
-        const userProfile = await UserModel.findOneAndUpdate({ username: req.params.username }, {
+        const bytes = CryptoJS.AES.decrypt( req.params.username, process.env.CRYPTO_SECRET);
+        const username = bytes.toString(CryptoJS.enc.Utf8);
+        const userProfile = await UserModel.findOneAndUpdate({ username:username }, {
             $set: {
-                isVerified:true
+                isVerified: true
             }
         }, { new: true });
-    
+
         res.redirect('/login')
     } catch (error) {
         res.status(500).send(error.message)
