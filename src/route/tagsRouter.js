a1bb859c0f53dc103eeabfd97db2ae5bb5510c51
sambare <boubasambare@yahoo.it>
2020-03-31 15:18:55 +0200

    post and get hastag

--- src/route/tagsRouter.js
+++ src/route/tagsRouter.js
@@ -6,9 +6,29 @@ const router = Router()
 
 router.get('/:hastag', async (req, res) => {
     try {
-        postWithHastag = postModel.find({tags: req.param.hastag})
+       let postWithTags = await postModel.find().where('tags').in(req.params.hastag)
+       if(!postWithTags)
+           return res.status(404).send(`there are no post with the hastag ${req.params.hastag}`)
 
-        res.json({post:postWithHastag})
+        res.send(postWithTags)
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+})
+
+router.post('/:postId', async (req, res) => {
+    try {
+        let tags = req.body.tags.split(',').map(singeleTag => singeleTag.trim())
+        
+        const newTags = await postModel.findByIdAndUpdate(req.params.postId, {
+            $addToSet: {
+                tags: {$each: tags}
+            }
+        }, {
+            new: true
+        })
+
+        res.send(newTags)
     } catch (error) {
         res.status(500).send(error.message)
     }
