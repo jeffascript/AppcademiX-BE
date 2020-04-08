d62d087c06ecff1a5f4159215fb8b48e516af267
unknown <plescan72@outlook.it>
2020-04-08 12:27:32 +0200

    reply fixed

--- src/route/replyRouter.js
+++ src/route/replyRouter.js
@@ -2,8 +2,7 @@ const express = require("express");
 const CommentModel = require("../model/commentSchema");
 const passport = require("passport");
 const router = express.Router();
-
-
+const {ObjectId} = require('mongodb')
 router.post(
   "/:commentId",
   passport.authenticate("jwt"),
@@ -13,11 +12,9 @@ router.post(
             ...req.body,
             userInfo:req.user._id
           }
-
           const comments = await CommentModel.findOne({_id: req.params.commentId })
           if (!comments)
             return res.status(500).send(`comment id ${req.params.commentId} not found`)
-      
           let newCreatedReply = await CommentModel.findByIdAndUpdate(req.params.commentId, {
             $push: {
                 replies: newReply
@@ -25,15 +22,12 @@ router.post(
         }, {
             new: true
         }).populate({ path: "replies.userInfo", model : "users"})
-
           res.send(newCreatedReply.replies)
-      
     } catch (error) {
       res.status(500).send(error.message);
     }
   }
 );
-
 router.get("/:commentId/:replyId", async (req, res) => {
   try {
       const singleReply = await CommentModel.findOne({
@@ -42,25 +36,28 @@ router.get("/:commentId/:replyId", async (req, res) => {
       }, {
           "replies.$": 1
       }).populate({ path: "replies.userInfo", model : "users"})
-
       res.status(200).send(singleReply)
   } catch (error) {
       res.status(500).send(error)
   }
 })
 
-router.patch("/:commentId/:replyId",passport.authenticate("jwt"),async (req, res, next) => {
+router.put("/:commentId/:replyId",passport.authenticate("jwt"),async (req, res) => {
   try {
+    let newReply = {
+      _id:req.params.replyId,
+      ...req.body,
+      userInfo:req.user._id
+    }
       const reply= await CommentModel.findOneAndUpdate({
           _id: req.params.commentId,
-          "replies._id": req.params.replyId
-      }, {"replies.$": req.body}, {new: true})
-      res.send("succefully edited!!!!")
+          "replies._id": new ObjectId(req.params.replyId) 
+      }, {"$set":{"replies": newReply}}, {new: true}).populate({ path: "replies.userInfo", model : "users"})
+      res.send(reply)
   } catch (error) {
-      res.status(500).send(error)
+      res.status(500).send(error.message)
   }
 })
-
 
 router.delete('/:commentId/:replyId', passport.authenticate("jwt") ,async (req, res) => {
   try {
@@ -76,5 +73,4 @@ router.delete('/:commentId/:replyId', passport.authenticate("jwt") ,async (req, 
       res.status(500).send(error)
   }
 })
-
 module.exports = router;
